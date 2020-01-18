2b705368d7983d3cca1eea960ca3368fc8ad7892
virgilt <travisjvirgil@gmail.com>
2020-01-18 18:35:31 -0500

    Setup server on port 5000

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,12 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
