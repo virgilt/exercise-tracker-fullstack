a2871c6bda0eed906c58113b0e5870197bf4197f
virgilt <travisjvirgil@gmail.com>
2020-01-19 14:18:20 -0500

    Create initial component structure

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class ExercisesList extends Component {
+    
+    render() {
+        return (
+            <div>
+                <p>You are on the Exercise List component!</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/navbar.component.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <Link className='navbar-brand' to='/'>ExerTracker</Link>
+            <div className='collapse navbar-collapse'>
+              <ul className='navbar-nav mr-auto'>
+                <li className='navbar-item'>
+                  <Link className='nav-link' to='/'>Exercises</Link>
+                </li>
+                <li className='navbar-item'>
+                  <Link className='nav-link' to='/create'>Create Exerices Log</Link>
+                </li>
+                <li className='navbar-item'>
+                  <Link className='nav-link' to='/user'>Create User</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>  
+        )
+    }
+}
