f9dee4b80199854ca005419f1f0c78d0ae04fae3
virgilt <travisjvirgil@gmail.com>
2020-01-18 19:38:08 -0500

    Connect server to MongDB database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+//Setup server on port 5000
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Parse JSON
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.Atlas_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log(`MongoDB database connection established successfully`)
+})
+
+//Listen on port
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
