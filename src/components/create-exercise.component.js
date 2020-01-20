2061fa7241f0fe96337dd978d60589b9116bc97b
virgilt <travisjvirgil@gmail.com>
2020-01-19 21:57:59 -0500

    Create edit and delete features for logged exercises

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
